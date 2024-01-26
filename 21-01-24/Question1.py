263d3335e7e7d2b77f6e1b2363e2b8e909409cfb
Varsha-09122003 <146991543+Varsha-09122003@users.noreply.github.com>
2024-01-25 22:24:11 -0800

    Update Question1.py

--- 21-01-24/Question1.py
+++ 21-01-24/Question1.py
@@ -43,12 +43,12 @@ import random
 import re 
 import sys
 if__name__=='__main__';
-   n=int(input())
-   if n%2==1:
-     print("Weird")
-   elif n%2==0 and 2<=n<=5:
-     print("Not Weird")
-   elif n%2==0 and 6<=n<=20:
-     print("Weird")
-   elif n%2==0 and n>20:
-     print("Not Weird")
+n = int(input())
+if n % 2 == 1:
+print("Weird")
+elif n % 2 == 0 and 2 <= n <= 5:
+print("Not Weird")
+elif n % 2 == 0 and 6 <= n <= 20:
+print("Weird")
+else:
+print("Not Weird")
