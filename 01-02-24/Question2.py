87ec65f8f1ab1ddfea5a60aeb40efe8e3d9d7f6f
Varsha-09122003 <146991543+Varsha-09122003@users.noreply.github.com>
2024-02-02 03:36:29 -0800

    Create Question2.py

---
+++ 01-02-24/Question2.py
@@ -0,0 +1,19 @@
+'''Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true'''
+
+  class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        elif sorted(s) == sorted(t):
+            return True
+        else:
+            return False
